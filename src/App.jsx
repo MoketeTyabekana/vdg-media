76dee69d8ddbcdf98dddd4fd6fc1c10a4cd4e9cb
Mokete <ematyabekana@gmail.com>
2025-01-21 15:54:43 +0200

    Created a components files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import './App.css'
+import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Services from "./components/Services"
+import Gallery from "./components/Gallery"
+import Footer from "./components/Footer"
 
 function App() {
- 
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <div className="min-h-screen">
+        <Navbar />
+        <Hero />
+        <Services />
+        <Gallery />
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
 export default App
+
