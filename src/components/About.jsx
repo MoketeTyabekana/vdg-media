e7d14b9138eb9b98e274539575798f3a6f0a0dab
Mokete <ematyabekana@gmail.com>
2025-01-21 19:42:25 +0200

    -Updated the hero section button -Created an About us and Gallery components.

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="lg:p-20  bg-gray-50">
+    <div className="max-w-full  ">
+      <h2 className="text-4xl font-bold text-center mb-12">Who We Are</h2>
+    </div>
+  </section>
+  )
+}
+
+export default About;
